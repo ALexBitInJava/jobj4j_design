5e3d0957ac9182730bf32b682fd9641fe7c0bcb8
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-07-01 14:40:48 +0300

    Что такое обобщенные типы (generics)

---
+++ src/main/java/ru/job4j/generics/Person.java
@@ -0,0 +1,15 @@
+package ru.job4j.generics;
+
+import java.util.Date;
+
+public class Person {
+    private String name;
+    private int age;
+    private Date date;
+
+    public Person(String name, int age, Date date) {
+        this.name = name;
+        this.age = age;
+        this.date = date;
+    }
+}
---
+++ src/main/java/ru/job4j/generics/Programmer.java
@@ -0,0 +1,10 @@
+package ru.job4j.generics;
+
+import java.util.Date;
+
+public class Programmer extends Person {
+
+    public Programmer(String name, int age, Date date) {
+        super(name, age, date);
+    }
+}
