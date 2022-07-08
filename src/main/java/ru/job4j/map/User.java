8544397c953f205d4b5b4cf4bcb18c1e85262c3c
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-07-08 15:31:44 +0300

    Без переопределения equals и hashCode

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,11 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Objects.hash;
 
 public class User {
     private String name;
@@ -11,5 +16,29 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        User us1 = new User("Alex", 1, new GregorianCalendar(2000, Calendar.JANUARY, 1));
+        User us2 = new User("Alex", 1, new GregorianCalendar(2000, Calendar.JANUARY, 1));
+        Map<User, Object> map1 = new HashMap<>();
+        map1.put(us1, new Object());
+        map1.put(us2, new Object());
+        System.out.println(map1.get(us1).hashCode());
+        System.out.println(hash(map1.get(us1)));
+        System.out.println(us1.hashCode());
+
+        System.out.println(map1.get(us2).hashCode());
+        System.out.println(hash(map1.get(us2)));
+        System.out.println(us2.hashCode());
+
+        int i1 = hash(map1.get(us1).hashCode()) & (map1.size() - 1);
+        System.out.println(i1);
+        int i2 = hash(map1.get(us2).hashCode()) & (map1.size() - 1);
+        System.out.println(i2);
+        System.out.println("--------------");
+        for (Map.Entry<User, Object> s : map1.entrySet()) {
+            System.out.println(s);
+        }
     }
 }
