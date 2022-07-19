63e09b05317558ee4918c277f700e7723ca3c3d0
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-07-20 01:05:02 +0300

    Анализ доступности сервера

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -3,26 +3,31 @@ package ru.job4j.io;
 import java.io.*;
 
 public class Analizy {
-    public static void unavailable(String source, String target) throws IOException {
+    private static boolean availability = true;
+    public static void unavailable(String source, String target) {
         try (BufferedReader reader = new BufferedReader(new FileReader(source));
-             PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+             PrintWriter printer = new PrintWriter(new FileOutputStream(target))) {
             StringBuilder log = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] array = line.split(" ");
-                if ("400".equals(array[0]) || "500".equals(array[0])) {
-                    log.append(array[1]).append(";");
-                }
-            }
-            out.println(log);
+            reader.lines().forEach(s -> checkFlag(log, s));
+            printer.println(log);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+
     public static void checkFlag(StringBuilder log, String line) {
+
         String[] array = line.split(" ");
-        if ("400".equals(array[0]) || "500".equals(array[0])) {
+        if (availability && ("400".equals(array[0]) || "500".equals(array[0]))) {
             log.append(array[1]).append(";");
+            availability = false;
+        }
+        if (!availability && ("200".equals(array[0]) || "300".equals(array[0]))) {
+            log.append(array[1]).append(";").append(System.lineSeparator());
+            availability = true;
         }
     }
+
     public static void main(String[] args) throws IOException {
         unavailable("./data/server.log", "./data/unavailable.csv");
     }
