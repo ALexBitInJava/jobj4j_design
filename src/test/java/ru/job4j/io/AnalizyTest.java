eda2cb87fe81e506f5cca0a54ca9390f9e4f5c87
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-07-20 22:51:32 +0300

    Сканирование файловой системы.

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -41,7 +41,7 @@ public class AnalizyTest {
     public void testUnavailable2() throws IOException {
         File source = folder2.newFile("server1.log");
         File target = folder2.newFile("unavailable.csv");
-        try (PrintWriter out = new PrintWriter(source)){
+        try (PrintWriter out = new PrintWriter(source)) {
             out.println("""
                     200 10:56:01
                     500 10:57:01
@@ -52,7 +52,7 @@ public class AnalizyTest {
         }
         Analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
         StringBuilder rsl = new StringBuilder();
-        try (BufferedReader in = new BufferedReader(new FileReader(target))){
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
             in.lines().forEach(rsl::append);
         }
         assertThat(rsl.toString(), is("10:57:01;11:02:02;"));
