f334de2a1c4e274ee72463ffa0175e25164719f0
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-07-22 22:53:06 +0300

    Архивировать проект

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -7,13 +7,13 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
-
     public void packFiles(List<Path> sources, File target) {
-        try (ZipOutputStream zipOutputStream = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(new BufferedOutputStream(
+                new FileOutputStream(target)))) {
             for (Path path : sources) {
                 zipOutputStream.putNextEntry(new ZipEntry(path.toFile().getPath()));
-                try (BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(path.toFile()))) {
-                    zipOutputStream.write(bufferedInputStream.readAllBytes());
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(path.toFile()))) {
+                    zipOutputStream.write(out.readAllBytes());
                 }
             }
         } catch (Exception e) {
@@ -21,34 +21,39 @@ public class Zip {
         }
     }
 
-    public static ArgsName zipValidation(String[] args) throws IllegalArgumentException {
-        if (args.length < 3) {
-            throw new IllegalArgumentException("Здесь должно быть не меньше 3 элементов");
+    public static void zipValidation(ArgsName argsName) {
+        argsName.get("o");
+        File file = new File(argsName.get("d"));
+        if (!file.exists()) {
+            throw new IllegalArgumentException("The file does not exist %s " + file.getAbsolutePath());
         }
-        ArgsName argsName = ArgsName.of(args);
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException("The directory does not exist %s " + file.isDirectory());
+        }
         if (!argsName.get("e").startsWith(".")) {
-            throw new IllegalArgumentException("Не начинается с точки");
+            throw new IllegalArgumentException("Does not start with '.' %s" + argsName.get("e"));
         }
-        if (!Path.of(argsName.get("d")).toFile().isDirectory()) {
-            throw new IllegalArgumentException("Директория не существует");
-        }
-
-        return argsName;
     }
 
-    public void packSingleFile(File source, File target) {
+    public void packSingleFile(List<Path> source, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
+            for (Path path : source) {
+                zip.putNextEntry(new ZipEntry(path.toString()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(path.toFile()))) {
+                    zip.write(out.readAllBytes());
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
-        Zip zip = new Zip();
-        zip.packSingleFile(new File("./pom.xml"), new File("./pom.zip"));
+    public static void main(String[] args) throws IOException {
+            ArgsName argsName = ArgsName.of(args);
+            zipValidation(argsName);
+            new Zip().packFiles(Search.search(Path.of(argsName.get("d")),
+                    path -> !path.toFile().getName().
+                            endsWith(argsName.get("e"))),
+                    new File(argsName.get("o")));
     }
 }
