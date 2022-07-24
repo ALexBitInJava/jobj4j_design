c41a9e4efa7860dea01951b13d2a4743c59b3fef
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-07-25 00:57:59 +0300

    Бот

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -13,11 +13,13 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        System.out.println(str);
-                        if ("http://localhost:9000/?msg=Bye".equals(in.readLine())) {
-                            server.close();
-                        }
+                    String reader = in.readLine();
+                    if (reader.contains("msg=Hello")) {
+                        out.write("Hello!\r\n\r\n".getBytes());
+                    } else if (reader.contains("msg=Exit")) {
+                        server.close();
+                    } else if (reader.contains("msg=Any")) {
+                        out.write("What?\r\n\r\n".getBytes());
                     }
                     out.flush();
                 }
