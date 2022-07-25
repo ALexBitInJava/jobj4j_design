0dbbe149a2372f99f42bbea539b365ef4fe4203d
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-07-25 16:05:20 +0300

    Slf4j - вывод exception

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -4,13 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class UsageLog4j {
+
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
         String name = "Petr Arsentev";
         int age = 33;
         LOG.debug("User info name : {}, age : {}", name, age);
-
         short s = 100;
         byte b = 10;
         long l = 100000L;
