c4a50eb96d14db0ed494ba6350c8c0ae3e693787
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-07-27 23:42:20 +0300

    pom

--- src/test/java/ru/job4j/collection/ForwardLinkedTest.java
+++ src/test/java/ru/job4j/collection/ForwardLinkedTest.java
@@ -1,57 +1,80 @@
 package ru.job4j.collection;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-class ForwardLinkedTest {
-    private ForwardLinked<Integer> linked;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
 
-    @BeforeEach
-    public void init() {
-        linked = new ForwardLinked<>();
-        linked.add(1);
-        linked.add(2);
-        linked.add(3);
-        linked.add(4);
-    }
+public class ForwardLinkedTest {
 
-    @Test
-    void whenDeleteFirst() {
-        assertThat(linked.deleteFirst()).isEqualTo(1);
-        assertThat(linked.deleteFirst()).isEqualTo(2);
-        assertThat(linked.deleteFirst()).isEqualTo(3);
-        assertThat(linked.deleteFirst()).isEqualTo(4);
-        assertThatThrownBy(linked.iterator()::next)
-                .isInstanceOf(NoSuchElementException.class);
-    }
-
-    @Test
-    void whenDeleteEmptyLinked() {
+    @Test(expected = NoSuchElementException.class)
+    public void whenDeleteFirst() {
         ForwardLinked<Integer> linked = new ForwardLinked<>();
-        assertThatThrownBy(linked::deleteFirst)
-                .isInstanceOf(NoSuchElementException.class);
-    }
-
-    @Test
-    void whenMultiDelete() {
+        linked.add(1);
         linked.deleteFirst();
-        Iterator<Integer> it = linked.iterator();
-        assertThat(it.next()).isEqualTo(2);
+        linked.iterator().next();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenDeleteEmptyLinked() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.deleteFirst();
     }
 
     @Test
-    void whenAddAndRevertTrue() {
+    public void whenMultiDelete() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
         linked.add(1);
         linked.add(2);
-        linked.add(3);
-        linked.add(4);
-        assertThat(linked).containsSequence(1, 2, 3, 4);
-        assertThat(linked.revert()).isTrue();
-        assertThat(linked).containsSequence(4, 3, 2, 1);
+        assertThat(linked.deleteFirst(), is(1));
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(2));
+    }
+
+    @Test
+    public void whenAddFirst() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.addFirst(2);
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(2));
+        assertFalse(it.hasNext());
+    }
+
+    @Test
+    public void whenAddThenIter() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.add(1);
+        linked.add(2);
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+    }
+
+    @Test
+    public void whenAddAndRevertThenIter() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.add(1);
+        linked.add(2);
+        linked.revert();
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(1));
+    }
+
+    @Test
+    public void whenSize0ThenReturnFalse() {
+        ForwardLinked<Integer> emptyList = new ForwardLinked<>();
+        assertFalse(emptyList.revert());
+    }
+
+    @Test
+    public void whenSize1ThenReturnFalse() {
+        ForwardLinked<Integer> singleList = new ForwardLinked<>();
+        singleList.add(1);
+        assertFalse(singleList.revert());
     }
 }
