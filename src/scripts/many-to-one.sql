3380c1757e8e29edf97e0854467783f6acfa58ea
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-08-01 15:56:33 +0300

    Связи между таблицами

--- src/scripts/many-to-one.sql
+++ src/scripts/many-to-one.sql
@@ -9,8 +9,8 @@ familyName_id int references surnameTable(id));
 
 insert into surnameTable(surname) values ('Jankins');
 
-insert into familyName (name) values('Alex', 1);
-insert into familyName (name) values('Juliana', 1);
+insert into familyName (name, familyName_id) values('Alex', 1);
+insert into familyName (name, familyName_id) values('Juliana', 1);
 
 select * from familyName;
 select * from surnameTable where id in (select id from familyName);
